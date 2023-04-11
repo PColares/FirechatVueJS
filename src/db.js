2be67c28b067a7c7ec05f68989980778b048c9d8
Pedro <65858913+PColares@users.noreply.github.com>
2023-04-11 02:27:22 -0300

    Update db.js
    Removing api key

--- src/db.js
+++ src/db.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-vSkxHpHDxjnHgd79cWkyPKdmLnRKuiE",
-  authDomain: "firechat-vue-56580.firebaseapp.com",
-  projectId: "firechat-vue-56580",
-  storageBucket: "firechat-vue-56580.appspot.com",
-  messagingSenderId: "996624417803",
-  appId: "1:996624417803:web:43e0149361b12d197a330e",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const db = firebase.initializeApp(firebaseConfig);
